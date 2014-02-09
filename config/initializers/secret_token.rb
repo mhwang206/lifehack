e194d9e30214a1b698ed799869b68be4273ea405
Matt Hwang <mhwang206@gmail.com>
2014-02-08 17:31:05 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lifehack::Application.config.secret_key_base = 'a70b5657206d4fbd26ca1b50cd9af71f2a1b5756ad27889bb8c3a55e47ffa070bd2b71a12128266fa02d63ab6b0ef05f9c5f635c5f9dbf40dbb8547c0a15383f'
